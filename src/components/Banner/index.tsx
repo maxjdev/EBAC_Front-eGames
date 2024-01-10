76de02d50c6b8e32460b61b8e9912a9ad64b383a
maxjdev <maxjramosdev@gmail.com>
2024-01-10 11:29:12 -0300

    add(component): Tag

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,13 +1,15 @@
 import { Image, Title, Prices } from './styles'
 import BannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
 
 const Banner = () => (
   <Image style={{ backgroundImage: `url(${BannerImg})` }}>
     <div className="container">
+      <Tag size="big">Highlight of the day</Tag>
       <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
       <Prices>
-        De <span>R$ 250,00</span> <br />
-        por apenas R$ 99,90
+        From <span>R$ 250,00</span> <br />
+        for just R$ 99,90
       </Prices>
     </div>
   </Image>
