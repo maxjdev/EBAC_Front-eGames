3e3e57c92278d6bfc05df446c1327f33ac4145e7
maxjdev <maxjramosdev@gmail.com>
2024-01-10 12:06:45 -0300

    add: React Router

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -7,13 +7,19 @@ const Banner = () => (
   <Image style={{ backgroundImage: `url(${BannerImg})` }}>
     <div className="container">
       <Tag size="big">Highlight of the day</Tag>
-      <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
-      <Prices>
-        From <span>R$ 250,00</span> <br />
-        for just R$ 99,90
-      </Prices>
-      <Button type="link" to="/product" title="">
-        Click here to take advantage of the offer
+      <div>
+        <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
+        <Prices>
+          From <span>R$ 250,00</span> <br />
+          for just R$ 99,90
+        </Prices>
+      </div>
+      <Button
+        type="link"
+        to="/product"
+        title="Click here to take advantage of the offer"
+      >
+        To enjoy
       </Button>
     </div>
   </Image>
