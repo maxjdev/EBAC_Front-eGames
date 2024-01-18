cef1702598a2cbf07cbd27f0a33a7a338c4030bc
maxjdev <maxjramosdev@gmail.com>
2024-01-18 16:37:38 -0300

    added GetTotalPrice

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,17 @@
+import { Game } from '../pages/Home'
+
 export const parseToBrl = (price = 0) => {
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL'
   }).format(price)
+}
+
+export const getTotalPrice = (items: Game[]) => {
+  return items.reduce((acumulator, currentItem) => {
+    if (currentItem.prices.current) {
+      return (acumulator += currentItem.prices.current)
+    }
+    return 0
+  }, 0)
 }
