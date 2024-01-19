07a9ee0ad5d43ce32d95595930b423e1c75286e1
maxjdev <maxjramosdev@gmail.com>
2024-01-19 12:25:47 -0300

    created clear Reducer

--- src/store/reducers/cart.ts
+++ src/store/reducers/cart.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { Game } from '../../pages/Home'
 
 type cartState = {
   items: Game[]
@@ -32,9 +31,12 @@ const cartSlice = createSlice({
     },
     close: (state) => {
       state.isOpen = false
+    },
+    clear: (state) => {
+      state.items = []
     }
   }
 })
 
-export const { add, close, open, remove } = cartSlice.actions
+export const { add, close, open, remove, clear } = cartSlice.actions
 export default cartSlice.reducer
